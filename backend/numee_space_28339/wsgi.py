6442daf1cce59be60b5d11476373e123909f7523
Crowdbotics <team@crowdbotics.com>
2021-07-01 01:03:09 +0000

    Initial commit.

---
+++ backend/numee_space_28339/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for numee_space_28339 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'numee_space_28339.settings')
+
+application = get_wsgi_application()
